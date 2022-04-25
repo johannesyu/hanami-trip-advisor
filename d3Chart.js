170687db3eb740889717d4ab0203ed9e3eb9ee59
johannesyu <johannesyu@berkeley.edu>
2022-04-25 16:30:07 -0700

    Updated csv

---
+++ d3Chart.js
@@ -0,0 +1,34 @@
+async function drawChart(container){
+   
+
+   //fetch and store the data 
+   var response = await fetch('https://raw.githubusercontent.com/johannesyu/hanami-trip-advisor/main/data/japan-topojson.json')
+	var country = await response.json()
+
+   response = await fetch('https://raw.githubusercontent.com/johannesyu/hanami-trip-advisor/main/data/overview.csv')
+	var csvString = await response.text()       
+
+   //parse csv
+   const parser = d3.dsvFormat(',')
+   
+   var hanamiSpots = parser.parse(csvString, d => ({
+      id: d.location_id,
+      name: d.name,
+      review_count: d.review_count,
+      rate: d.rate,
+      city: d.city,
+      region_jp: d.region_jp,
+      region_en: d.region_en,
+      address: d.address,
+      lat: +d.latitude,
+      long: +d.longitude,
+      href: d.href,
+      setting: d.surrounding,
+      attr: d.attraction,
+      }))
+      
+   console.log(hanamiSpots)
+
+}
+
+
