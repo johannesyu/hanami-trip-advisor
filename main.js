2b2fde1479515f689ecd364679a6b8fd20244850
johannesyu <johannesyu@berkeley.edu>
2022-05-01 13:41:07 -0700

    Revert "Modified tooltips"
    This reverts commit f55540285f1d37214df1858beca4be5d87154e22.

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
       drawChart();
-
-var tooltipSpan = document.getElementById('quote-tooltiptext');
-
-window.onmousemove = function (e) {
-    var x = e.clientX,
-        y = e.clientY;
-    tooltipSpan.style.top = (y + 20) + 'px';
-    tooltipSpan.style.left = (x + 20) + 'px';
-};
